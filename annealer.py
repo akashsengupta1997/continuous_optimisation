a6abfb541f75477aed7d206a3af35e633894a3df
Akash Sengupta <akash.sengupta1997@gmail.com>
2018-12-15 11:52:50 +0800

    added code to visualise search patterns

--- annealer.py
+++ annealer.py
@@ -1,16 +1,19 @@
 import numpy as np
 import math
-from objective import schwefel_func
 
 
 class SimpleAnnealer:
     def __init__(self, objective_func, num_control_vars):
         self.objective_func = objective_func
         self.num_control_vars = num_control_vars
+        self.allowed_evals = 10000
+        self.initial_temperature_search_evals = 700
 
         # Max allowed change in each control variable for simple soln generator
         # TODO mess with sizes of diagonals
-        self.C = 10*np.identity(num_control_vars)
+        self.C = 250*np.identity(num_control_vars)
+        # Temperature multiplier
+        self.alpha = 0.95
 
     def soln_generator(self, current_soln):
         """
@@ -22,13 +25,16 @@ class SimpleAnnealer:
         :param current_soln:
         :return:
         """
-        u = np.random.uniform(-1.0, 1.0, self.num_control_vars)
         valid_solution = False
         while not valid_solution:
+            u = np.random.uniform(-1.0, 1.0, self.num_control_vars)
             new_soln = current_soln + np.dot(self.C, u)
             if np.all(new_soln <= 500) and np.all(new_soln >= -500):
                 valid_solution = True
         return new_soln
+
+    def random_soln_generator(self):
+        return 500*np.random.uniform(-1.0, 1.0, self.num_control_vars)
 
     def acceptance_probability(self, current_f_val, new_fval, temperature):
         delta_f = new_fval - current_f_val
@@ -39,16 +45,39 @@ class SimpleAnnealer:
             acceptance_prob = math.exp(- delta_f / temperature)
             return acceptance_prob
 
+    def initial_temperature_search(self, initial_acceptance_prob):
+        current_soln = self.random_soln_generator()
+        print('init search init soln', current_soln)
+        current_fval = self.objective_func(current_soln)
+        fval_increases = []
+
+        for i in range(self.initial_temperature_search_evals - 1):
+            # need -1 here because doing 1 function evaluation outside the loop
+            new_soln = self.soln_generator(current_soln)
+            new_fval = self.objective_func(current_soln)
+            delta_f = new_fval - current_fval
+            if delta_f > 0:
+                fval_increases.append(delta_f)
+            current_soln = new_soln.copy()
+            current_fval = new_fval
+
+        initial_temperature = - np.mean(fval_increases) / math.log(initial_acceptance_prob)
+        # print('mean fval increase', np.mean(fval_increases))
+        # print('T0', initial_temperature)
+        return initial_temperature
+
     def simulated_annealing(self):
 
-        current_soln = [300, 300]  # TODO randomise initial solution
+        current_soln = self.random_soln_generator()
+        print('init soln', current_soln)
         current_fval = self.objective_func(current_soln)
         best_soln = current_soln
         best_fval = current_fval
-        temperature = 1000  # TODO initial search for temperature
+        temperature = self.initial_temperature_search(0.8)
         soln_store = []
 
-        for i in range(10000):
+        for i in range(self.allowed_evals - self.initial_temperature_search_evals - 1):
+            # need -1 here because doing 1 function evaluation outside the loop
             new_soln = self.soln_generator(current_soln)
             new_fval = self.objective_func(new_soln)
             accept_prob = self.acceptance_probability(current_fval, new_fval, temperature)
@@ -62,8 +91,10 @@ class SimpleAnnealer:
                     best_soln = current_soln.copy()
                     best_fval = current_fval
 
-            temperature = 0.95*temperature
-            # print(new_fval)
+
+            
+
+            temperature = self.alpha * temperature
 
         return best_soln, best_fval, soln_store
 
---
+++ visualise.py
@@ -0,0 +1,51 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+
+def surface_plot(objective_func):
+    x = np.arange(-500, 500, 0.5)
+    y = np.arange(-500, 500, 0.5)
+    xx, yy = np.meshgrid(x, y, sparse=False)
+    z = np.zeros((len(x), len(y)))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z[i, j] = objective_func([xx[i, j], yy[i, j]])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(xx, yy, z, cmap=cm.coolwarm)
+    plt.show()
+
+
+def contour_plot(objective_func):
+    x = np.arange(-500, 500, 1)
+    y = np.arange(-500, 500, 1)
+    xx, yy = np.meshgrid(x, y, sparse=False)
+    z = np.zeros((len(x), len(y)))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z[i, j] = objective_func([xx[i, j], yy[i, j]])
+
+    # plt.figure()
+    plt.contour(xx, yy, z, 10)
+    plt.show()
+
+
+def search_pattern_contour_plot(objective_func, solns):
+    x = np.arange(-500, 500, 1)
+    y = np.arange(-500, 500, 1)
+    xx, yy = np.meshgrid(x, y, sparse=False)
+    z = np.zeros((len(x), len(y)))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z[i, j] = objective_func([xx[i, j], yy[i, j]])
+
+    x1s = [coord[0] for coord in solns]
+    x2s = [coord[1] for coord in solns]
+
+    plt.scatter(x1s, x2s, facecolors='none', edgecolors='r')
+    plt.contour(xx, yy, z, 10)
+
+    plt.show()
